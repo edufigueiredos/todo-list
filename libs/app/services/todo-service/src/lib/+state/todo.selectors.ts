3e4bf2826cbd29417bc57d631014fe0c6f838be7
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-03-01 02:14:43 -0300

    Implementando um selector que recebe parâmetro.

--- libs/app/services/todo-service/src/lib/+state/todo.selectors.ts
+++ libs/app/services/todo-service/src/lib/+state/todo.selectors.ts
@@ -7,4 +7,12 @@ export const todoSelector = createFeatureSelector<TodoState>(todoStateKey);
 export const allTodosSelector = createSelector(
   todoSelector,
   (state) => state.todos
+);
+
+export const getTodoSelector = (id: string) => createSelector(
+  allTodosSelector,
+  (state) => {
+    const index = state.findIndex(todo => todo._id === id);
+    return state[index];
+  }
 )
