bca67e5ed5a50a8eea65e470fa6a2fdb42e4ce21
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-03-02 12:23:45 -0300

    Implementing other effects

--- libs/app/services/todo-service/src/lib/+state/todo.actions.ts
+++ libs/app/services/todo-service/src/lib/+state/todo.actions.ts
@@ -2,14 +2,14 @@ import { createAction, props } from '@ngrx/store'
 import { Todo } from '@todo-list/schema/todo';
 
 export const setAllTodosStore = createAction('[Todo] Carrega todas as tarefa no Store', props<{ todos: Todo[] }>());
-export const setTodoStore = createAction('[Todo] Adiciona uma tarefa no Store', props<{todo: Todo}>());
-export const removeTodoStore = createAction('[Todo] Remove uma tafera do Store', props<{id: string}>());
+export const setTodoStore = createAction('[Todo] Adiciona uma tarefa no Store', props<{ todo: Todo }>());
+export const removeTodoStore = createAction('[Todo] Remove uma tafera do Store', props<{ id: string }>());
 export const successAction = createAction('[Todo] Sucesso');
 export const errorAction = createAction('[Todo] Erro');
-export const updateTodoStore = createAction('[Todo] Atualiza uma todo no Store', props<{id: string, todo: Todo}>());
+export const updateTodoStore = createAction('[Todo] Atualiza uma todo no Store', props<{ id: string, todo: Todo }>());
 export const loadTodosEffect = createAction('[Todo] Carregar tarefas');
-export const loadTodoByIdEffect = createAction('[Todo] Carrega tarefa por ID', props<{ id: string }>());
+// export const loadTodoByIdEffect = createAction('[Todo] Carrega tarefa por ID', props<{ id: string }>());
 export const createTodoEffect = createAction('[Todo] Cria uma tarefa', props<{ todo: Todo }>());
 export const updateTodoEffect = createAction('[Todo] Atualiza uma tarefa', props<{ id: string, todo: Todo }>());
 export const completeTodoEffect = createAction('[Todo] Completa uma tarefa', props<{ id: string, date: { date: Date } }>());
-export const deleteTodoEffect = createAction('[Todo] Apaga uma tarefa', props<{id: string}>());
+export const deleteTodoEffect = createAction('[Todo] Apaga uma tarefa', props<{ id: string }>());
--- libs/app/services/todo-service/src/lib/+state/todo.effects.ts
+++ libs/app/services/todo-service/src/lib/+state/todo.effects.ts
@@ -1,9 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { TodoService } from "../todo.service";
-import { errorAction, loadTodosEffect, setAllTodosStore, successAction } from './todo.actions';
+import {
+  errorAction,
+  loadTodosEffect,
+  setAllTodosStore,
+  successAction, createTodoEffect,
+  setTodoStore,
+  updateTodoEffect,
+  updateTodoStore,
+  deleteTodoEffect,
+  completeTodoEffect
+} from './todo.actions';
 import { catchError, map, switchMap, tap } from 'rxjs';
 import { Store } from "@ngrx/store";
+import { Todo } from "@todo-list/schema/todo";
 
 @Injectable()
 export class TodoEffect {
@@ -12,12 +23,54 @@ export class TodoEffect {
     () => this.actions$.pipe(
       ofType(loadTodosEffect),
       switchMap(() => this.todoService.getAll().pipe(
-        tap(todos => this.store.dispatch(setAllTodosStore({todos})))
+        tap((todos: Todo[]) => this.store.dispatch(setAllTodosStore({ todos })))
       )),
       map(() => successAction()),
       catchError(() => [errorAction()])
     )
-  )
+  );
+
+  createTodo$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(createTodoEffect),
+      switchMap(({ todo }) => this.todoService.create(todo).pipe(
+        tap((todo: Todo) => this.store.dispatch(setTodoStore({ todo })))
+      )),
+      map(() => successAction()),
+      catchError(() => [errorAction()])
+    )
+  );
+
+  updateTodo$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(updateTodoEffect),
+      switchMap(({ id, todo: todoToUpdate }) => this.todoService.update(id, todoToUpdate).pipe(
+        tap((todo: Todo) => this.store.dispatch(updateTodoStore({ id, todo })))
+      )),
+      map(() => successAction()),
+      catchError(() => [errorAction])
+    )
+  );
+
+  deleteTodo$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(deleteTodoEffect),
+      switchMap(({ id }) => this.todoService.delete(id)),
+      map(() => successAction()),
+      catchError(() => [errorAction])
+    )
+  );
+
+  completeTodo$ = createEffect(
+    () => this.actions$.pipe(
+      ofType(completeTodoEffect),
+      switchMap(({ id, date }) => this.todoService.completeTask(id, date).pipe(
+        tap((todo: Todo) => this.store.dispatch(updateTodoStore({ id, todo })))
+      )),
+      map(() => successAction()),
+      catchError(() => [errorAction])
+    )
+  );
 
   constructor(
     private actions$: Actions,
