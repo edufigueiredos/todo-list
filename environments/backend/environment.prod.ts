09d0f8f8d18cc758bab38f36c8290767ad5bd865
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-03-19 01:12:54 -0300

    Adding configuration to deploy the API

--- environments/backend/environment.prod.ts
+++ environments/backend/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  jwt_secret: 'senhaSuperSegura',
+  jwt_expire: '30d',
+  mongodbConnectionString: 'mongodb+srv://dudugama:DMmUaf8VSo2ALuN6@edufigueiredos-todo-app.ci4lx.mongodb.net/todo-list'
 }
--- environments/backend/environment.ts
+++ environments/backend/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   jwt_secret: 'senhaSuperSegura',
-  jwt_expire: '30d'
+  jwt_expire: '30d',
+  mongodbConnectionString: 'mongodb://localhost:27017/todo-list'
 }
