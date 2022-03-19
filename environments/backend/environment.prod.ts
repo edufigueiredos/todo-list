ae8b5f99f9a2d84d39e769846fb0be6fbb9f77ab
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-03-19 02:09:52 -0300

    Try to add a new enable CORS

--- environments/backend/environment.prod.ts
+++ environments/backend/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   jwt_secret: 'senhaSuperSegura',
   jwt_expire: '30d',
   mongodbConnectionString: 'mongodb+srv://dudugama:DMmUaf8VSo2ALuN6@edufigueiredos-todo-app.ci4lx.mongodb.net/todo-list',
-  enabledCors: 'https://edfs-todo-list-api.herokuapp.com/api/*'
+  enabledCors: 'https://edfs-todo-list-api.herokuapp.com'
 }
